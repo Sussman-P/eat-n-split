574b2a19f1e1a6ff3fbee3c0ea3614f92ee3cb24
Phillip Sussman <phillipsussman@gmail.com>
2024-10-17 11:13:30 -0700

    Added UI, and functionality.

--- src/App.js
+++ src/App.js
@@ -30,5 +30,37 @@ export default function App() {
 }
 
 function FriendList() {
-  return <div>List</div>;
+  const friends = initialFriends;
+
+  return (
+    <div>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </div>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.img} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}.
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}.
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
+      <button className="button">Select</button>
+    </li>
+  );
 }
